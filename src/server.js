52fc2a19daed3a8e44bb3e42b31ff5814e65a206
Joe Davitt <joseph.j.davitt@gmail.com>
2023-02-22 23:42:33 +0000

    created v2 route

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ const errorHandler = require('./error-handlers/500.js');
 const notFound = require('./error-handlers/404.js');
 const authRoutes = require('./auth/routes.js');
 const v1Routes = require('./routes/v1.js');
+const v2Routes = require('./routes/v2.js');
 
 // Prepare the express app
 const app = express();
@@ -26,6 +27,7 @@ app.use(authRoutes);
 // assuming you are on port 3001:
 // http://localhost:3001/api/v1/food
 app.use('/api/v1', v1Routes);
+app.use('/api/v2', v2Routes);
 
 // Catchalls
 app.use(notFound);
