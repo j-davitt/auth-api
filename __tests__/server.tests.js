3890da15a030704c41ad13d23f13fb96a9a50f7c
Joe Davitt <joseph.j.davitt@gmail.com>
2023-02-23 18:45:05 +0000

    update tests

--- __tests__/server.tests.js
+++ __tests__/server.tests.js
@@ -1,19 +1,14 @@
 'use strict';
 
 const { server } = require('../src/server');
-const { db, users } = require('../src/models');
+const { db } = require('../src/models');
 const supertest = require('supertest');
 const request = supertest(server);
 
-let testWriter;
+
 
 beforeAll(async () => {
   await db.sync();
-  testWriter = await users.create({
-    username: 'testWriter',
-    password: 'pass123',
-    role: 'writer',
-  });
 });
 
 afterAll(async () => {
@@ -40,13 +35,5 @@ describe('Server tests', () => {
     expect(response.body.user.username).toEqual('testAdmin');
 
   });
-
-  it('allows read access', async () => {
-    let response = await request.get('/api/v2/food').set('Authorization', `Bearer ${testWriter.token}`);
-
-    expect(response.status).toEqual(200);
-
-  });
-
 
 });
--- __tests__/v2.test.js
+++ __tests__/v2.test.js
@@ -1,50 +1,58 @@
 'use strict';
 
 const { server } = require('../src/server');
-const { db } = require('../src/models');
+const { db, users } = require('../src/models');
 const supertest = require('supertest');
 const request = supertest(server);
 
-
+let testAdmin;
 
 beforeAll(async () => {
   await db.sync();
+  testAdmin = await users.create({
+    username: 'admin',
+    password: 'password',
+    role: 'admin',
+  });
 });
 
 afterAll(async () => {
   await db.drop();
 });
 
-describe('v1 Routes', () => {
+describe('v2 Routes', () => {
   it('creates a record', async () => {
-    let response = await request.post('/api/v1/food').send({
+    let response = await request.post('/api/v2/food').send({
       name: 'test',
       calories: '100',
       type: 'protein',
-    });
+    }).set('Authorization', `Bearer ${testAdmin.token}`);
 
     expect(response.status).toEqual(201);
     expect(response.body.name).toEqual('test');
   });
+
   it('gets all records', async () => {
-    let response = await request.get('/api/v1/food');
+    let response = await request.get('/api/v2/food').set('Authorization', `Bearer ${testAdmin.token}`);
 
     expect(response.status).toEqual(200);
     expect(response.body.length).toBeGreaterThan(0);
     expect(response.body[0].name).toEqual('test');
   });
+
   it('updates a record', async () => {
-    let response = await request.put('/api/v1/food/1').send({
+    let response = await request.put('/api/v2/food/1').send({
       name: 'test2',
       calories: '100',
       type: 'protein',
-    });
+    }).set('Authorization', `Bearer ${testAdmin.token}`);
 
     expect(response.status).toEqual(200);
     expect(response.body.name).toEqual('test2');
   });
+  
   it('deletes a record', async () => {
-    let response = await request.delete('/api/v1/food/1');
+    let response = await request.delete('/api/v2/food/1').set('Authorization', `Bearer ${testAdmin.token}`);
 
     expect(response.status).toEqual(200);
   });
